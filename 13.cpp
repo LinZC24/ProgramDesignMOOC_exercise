6276f5b1c09b0cbfc5833bb85e2e276c03d32f07
LinZC24 <1150609717@qq.com>
2023-06-26 13:54:03 +0800

    added t14, t13 unfinished

--- 13.cpp
+++ 13.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 string redWarrior[5] {"dragon", "lion", "wolf", "ninja", "iceman"};
 string blueWarrior[5] {"wolf", "dragon", "ninja", "iceman", "wolf"};
+map<string, int> warriorHP;
 
-class operation;
 class base {
   private:
     int hp;
@@ -15,22 +15,37 @@ class base {
   public:
     bool stop = false;
     base() {}
-    base(int currentBaseHP):hp(currentBaseHP) {}
+    base(int currentBaseHP):hp(currentBaseHP) {
+      for(int i = 0; i < 5; i++) {
+        canBuild[i] = true;
+      }
+    }
     //void build(int hp) {}
-    void build(int clock) {
+    void nextID(int & id) {
+      id++;
+    }
+    bool buildCheck(int & id, map<string, int> warriorHP) {
+      
+    }
+    void build(int clock, int & id) {
+      if(buildCheck(id, warriorHP)) {
 
+      }
     }
     ~base() {}
 };
 
 class operation {
   private:
-    int redID, blueID, clock;
   public:
     base redBase;
     base blueBase;
+    int redID, blueID, clock;
     operation() {}
     operation(int m):redBase(m), blueBase(m), redID(0), blueID(0), clock(0) {}
+    void add() {
+      clock++;
+    }
 };
 
 int main() {
@@ -39,7 +54,6 @@ int main() {
   for(int i = 1; i <= n; i++) {
     int m, dragonHP, ninjaHP, icemanHP, lionHP, wolfHP;
     cin >> m >> dragonHP >> ninjaHP >> icemanHP >> lionHP >> wolfHP;
-    map<string, int> warriorHP;
     warriorHP["dragon"] = dragonHP;
     warriorHP["ninja"] = ninjaHP;
     warriorHP["iceman"] = icemanHP;
@@ -48,7 +62,11 @@ int main() {
     cout << "Case:" << i << endl;
     operation o(m);
     while(!o.redBase.stop && !o.blueBase.stop) {
-
+      o.redBase.build(o.clock, o.redID);
+      o.redBase.nextID(o.redID);
+      o.blueBase.build(o.clock, o.blueID);
+      o.blueBase.nextID(o.blueID);
+      o.add();
     }
   }
 }
